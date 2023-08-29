e5942c732ca49e38ca0f1c5fc94da83174fec825
JAY JAIN <110301971+Jay-talera@users.noreply.github.com>
2023-08-30 00:43:16 +0530

    Time: 2248 ms (40.68%) | Memory: 0B (100.00%) - LeetSync

--- SQL/577-employee-bonus/employee-bonus.sql
+++ SQL/577-employee-bonus/employee-bonus.sql
@@ -1,1 +1,1 @@
-select e.name,b.bonus from employee e left join bonus b on e.empid=b.empid where coalesce(b.bonus,0)<1000
+select e.name,b.bonus from employee e left join bonus b on e.empid=b.empid where b.bonus<1000 or b.bonus is null
