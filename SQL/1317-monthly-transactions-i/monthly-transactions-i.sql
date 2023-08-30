f126f0bcc8641f1602b56a5348e4d4d0a41cc384
JAY JAIN <110301971+Jay-talera@users.noreply.github.com>
2023-08-31 00:29:01 +0530

    Time: 1669 ms (8.90%) | Memory: 0B (100.00%) - LeetSync

---
+++ SQL/1317-monthly-transactions-i/monthly-transactions-i.sql
@@ -0,0 +1,5 @@
+select left(trans_date,7)AS month,
+country,count(country) as trans_count,
+sum(if(state='approved',1,0)) as approved_count,
+sum(amount) as trans_total_amount,
+sum(if(state='approved',amount,0))as approved_total_amount from transactions group by month,country
