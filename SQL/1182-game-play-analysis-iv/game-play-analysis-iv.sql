9e93922f45a993f530702cf46d38ae766fd325a2
JAY JAIN <110301971+Jay-talera@users.noreply.github.com>
2023-09-01 00:04:37 +0530

    Time: 1819 ms (6.05%) | Memory: 0B (100.00%) - LeetSync

---
+++ SQL/1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -0,0 +1,2 @@
+SELECT ROUND(COUNT(DISTINCT player_id )/(select count(distinct player_id) from activity),2) as fraction from activity where 
+(player_id,DATE_SUB(event_date,INTERVAL 1 DAY)) in (select player_id,min(event_date) from activity group by player_id)
