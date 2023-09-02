a85143ee3a7484de687e168d64bdd2b4fa2514d9
JAY JAIN <110301971+Jay-talera@users.noreply.github.com>
2023-09-02 08:15:13 +0530

    Time: 35 ms (90.46%) | Memory: 16.3 MB (60.33%) - LeetSync

--- leetcode-solutions/258-add-digits/add-digits.py
+++ leetcode-solutions/258-add-digits/add-digits.py
@@ -1,9 +1,8 @@
 class Solution:
     def addDigits(self, num: int) -> int:
-        while num>9:
-            sum=0
-            while num:
-                sum+=num%10
-                num=num//10
-            num=sum
-        return num
+        if num==0:
+            return 0
+        elif num%9==0: 
+            return 9
+        else:
+            return (num%9)
