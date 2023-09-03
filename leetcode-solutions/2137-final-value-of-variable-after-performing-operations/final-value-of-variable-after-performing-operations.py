1b433175bb94a1b45e9ee0416b973a8001cf5a28
JAY JAIN <110301971+Jay-talera@users.noreply.github.com>
2023-09-03 10:59:06 +0530

    Time: 50 ms (97.58%) | Memory: 16.3 MB (37.24%) - LeetSync

--- leetcode-solutions/2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.py
+++ leetcode-solutions/2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.py
@@ -2,7 +2,7 @@ class Solution:
     def finalValueAfterOperations(self, operations: List[str]) -> int:
         x=0
         for i in operations:
-            if i in ('++X','X++'):
+            if i == 'X++' or i=='++X':
                 x+=1
             else:
                 x-=1
