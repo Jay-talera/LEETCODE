8618eef24bc931043539f10b218c92230befc2d4
JAY JAIN <110301971+Jay-talera@users.noreply.github.com>
2023-09-05 07:47:41 +0530

    Time: 111 ms (45.88%) | Memory: 16.7 MB (44.11%) - LeetSync

---
+++ leetcode-solutions/1677-matrix-diagonal-sum/matrix-diagonal-sum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def diagonalSum(self, mat: List[List[int]]) -> int:
+        n=len(mat)
+        mid=n//2
+        res=0
+        for i in range(len(mat)):
+            res+=mat[i][i]
+            res+=mat[n-1-i][i]
+
+        if n%2==1:
+            res -= mat[mid][mid]
+        return res
