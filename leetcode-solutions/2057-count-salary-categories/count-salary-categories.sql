91a2f666d9f2767f1350a214fe29a6067a9a7963
JAY JAIN <110301971+Jay-talera@users.noreply.github.com>
2023-09-05 16:15:16 +0530

    Time: 4089 ms (5.02%) | Memory: 0B (100.00%) - LeetSync

---
+++ leetcode-solutions/2057-count-salary-categories/count-salary-categories.sql
@@ -0,0 +1,6 @@
+select "Low Salary" as category,sum(income<20000) as accounts_count from accounts
+union
+select "Average Salary" as category,sum(income>=20000 and income <=50000) as accounts_count from accounts
+union
+select "High Salary" as category,sum(income>50000) as accounts_count from accounts
+order by accounts_count
