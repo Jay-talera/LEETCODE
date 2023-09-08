aef3fdd15a2728fac4280507878097dcff334177
JAY JAIN <110301971+Jay-talera@users.noreply.github.com>
2023-09-08 10:11:58 +0530

    Time: 92 ms (63.68%) | Memory: 16.6 MB (72.96%) - LeetSync

--- leetcode-solutions/4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ leetcode-solutions/4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -1,10 +1,17 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        nums1.extend(nums2)
-        nums1.sort()
-        if len(nums1)%2==1:
-            s=(len(nums1)//2)
-            return nums1[s]
-        else:
-            s=(len(nums1)//2)
-            return (nums1[s]+nums1[s-1])/2
+        m, n = len(nums1), len(nums2)
+        mid = (m + n) // 2 + 1
+        prev2 = prev1 = None
+        i = j = 0
+
+        for _ in range(mid):
+            prev2 = prev1
+            if j == n or (i != m and nums1[i] <= nums2[j]):
+                prev1 = nums1[i]
+                i += 1
+            else:
+                prev1 = nums2[j]
+                j += 1
+        
+        return prev1 if (m + n) % 2 else (prev1 + prev2) / 2
