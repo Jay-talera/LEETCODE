e6a7deb0b883726e9a0986035e6d725a8a2357f2
JAY JAIN <110301971+Jay-talera@users.noreply.github.com>
2023-09-08 08:40:57 +0530

    Time: 34 ms (93.54%) | Memory: 16.2 MB (94.68%) - LeetSync

--- leetcode-solutions/118-pascals-triangle/pascals-triangle.py
+++ leetcode-solutions/118-pascals-triangle/pascals-triangle.py
@@ -2,11 +2,6 @@ class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
         
         res=[[1]]
-
         for i in range(numRows-1):
-            temp=[0]+res[-1]+[0]
-            row=[]
-            for j in range (len(res[-1])+1):
-                row.append(temp[j]+temp[j+1])
-            res.append(row)
-        return res
+            res +=[list(map(lambda x, y: x+y, res[-1] + [0] , [0] + res[-1]))]
+        return res[:numRows]
